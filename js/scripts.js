1bbe3557029bce2de604ffd61bc70c2408bbf2df
joceilton <joceilton.g@gmail.com>
2021-02-16 14:52:56 -0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,23 @@
 $(function() {
 
+	AOS.init({
+	  duration: 1200,
+	})
+
+	setTimeout(function() {
+		$(".loading").fadeOut('slow')
+	}, 5000)
+
     function abre_menu() {
         $(".menu a").slideToggle('slow')
     }
 
     $(".btn_menu").on("click", function() {
         abre_menu()
+    })
+
+    $(".menu a").click(function() {
+    	abre_menu()
     })
 
 })
